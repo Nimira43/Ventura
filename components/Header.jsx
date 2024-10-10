a6524175e5bdd019ad9a865d11e94bc07e02c78a
Nimira43 <n.rabone72@aol.com>
2024-10-11 00:44:00 +0100

    Changed a tags to Link in Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,58 +1,60 @@
+import Link from 'next/link'
+
 const Header = () => {
   return ( 
     <header className="bg-[#87ceeb]">
       <nav className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <div className="flex items-center">
-            <a href="/">
+            <Link href="/">
               <h1 className="h-12 w-12 p-3">VV</h1>
-            </a>
+            </Link>
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
-                <a
+                <Link
                   href="/"
                   className="rounded-md px-3 py-2 text-sm font-medium text-[#333] hover:bg-[#2947be] hover:text-[#f5f5dc]"
                 >
                   Rooms
-                </a>
-                <a
+                </Link>
+                <Link
                   href="/bookings.html"
                   className="rounded-md px-3 py-2 text-sm font-medium text-[#333] hover:bg-[#2947be] hover:text-[#f5f5dc]"
                 >
                   Bookings
-                </a>
-                <a
+                </Link>
+                <Link
                   href="/add-room.html"
                   className="rounded-md px-3 py-2 text-sm font-medium text-[#333] hover:bg-[#2947be] hover:text-[#f5f5dc]"
                 >
                   Add Room
-                </a>
+                </Link>
               </div>
             </div>
           </div>
           <div className="ml-auto">
             <div className="ml-4 flex items-center md:ml-6">
-              <a
+              <Link
                 href="login.html"
                 className="mr-3 text-[#333] hover:text-[#2947be]"
               >
                 <i className="fa fa-sign-in"></i> Login
-              </a>
-              <a
+              </Link>
+              <Link
                 href="register.html"
                 className="mr-3 text-[#333] hover:text-[#2947be]"
               >
                 <i className="fa fa-user"></i> Register
-              </a>
-              <a href="my-rooms.html">
+              </Link>
+              <Link href="my-rooms.html">
                 <i className="fa fa-building"></i> My Rooms
-              </a>
-              <a
+              </Link>
+              <Link
                 href="login.html"
                 className="mx-3 text-[#333] hover:text-[#2947be]"
               >
                 <i className="fa fa-sign-out"></i> Sign Out
-              </a>
+              </Link>
             </div>
           </div>
         </div>
@@ -60,24 +62,24 @@ const Header = () => {
 
       <div className="md:hidden">
         <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-          <a
+          <Link
             href="/"
             className="block rounded-md px-3 py-2 text-base font-medium text-[#333] hover:bg-[#2947be] hover:text-[#f5f5dc]"
           >
             Rooms
-          </a>
-          <a
+          </Link>
+          <Link
             href="/bookings.html"
             className="block rounded-md px-3 py-2 text-base font-medium text-[#333] hover:bg-[#2947be] hover:text-[#f5f5dc]"
           >
             Bookings
-          </a>
-          <a
+          </Link>
+          <Link
             href="/add-room.html"
             className="block rounded-md px-3 py-2 text-base font-medium text-[#333] hover:bg-[#2947be] hover:text-[#f5f5dc]"
           >
             Add Room
-          </a>
+          </Link>
         </div>
       </div>
     </header>

74ea3514162f72a227351ebe35209e269e7055ab
Nimira43 <n.rabone72@aol.com>
2024-10-13 17:05:44 +0100

    Made year in footer dynamic using getFullYear()

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,11 @@
 const Footer = () => {
+const currenYear = new Date().getFullYear()
+
   return ( 
     <footer className="bg-[#333] py-6">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <p className="text-center text-sm text-[#f5f5dc]">
-          &copy; 2024 Ventura Venues. All rights reserved.
+          &copy; { currenYear } Ventura Venues. All rights reserved.
         </p>
       </div>
     </footer>

2609f1286fd579fdf48614f2b58bc2ffa4c53ac4
Nimira43 <n.rabone72@aol.com>
2024-11-05 23:46:27 +0000

    Made indiviual room dynamic. Created new component for Booking Form.

--- app/rooms/[id]/page.jsx
+++ app/rooms/[id]/page.jsx
@@ -1,4 +1,8 @@
 import Heading from '@/components/Heading'
+import BookingForm from '@/components/BookingForm'
+import Image from 'next/image'
+import Link from 'next/link'
+import { LuChevronLeftCircle } from 'react-icons/lu'
 import rooms from '@/data/rooms.json'
 
 const RoomPage = ({ params }) => {
@@ -13,121 +17,45 @@ const RoomPage = ({ params }) => {
     <>
       <Heading title={ room.name } />
       <div className="bg-[#fff] shadow rounded-lg p-6">
-        <a
-          href="/rooms.html"
+        <Link
+          href="/"
           className="flex items-center text-[#333] hover:text-[#111] mb-4"
         >
-          <i className="fa fa-chevron-left"></i>
+          <LuChevronLeftCircle className='inline mr-1' />
           <span className="ml-2">Back to Rooms</span>
-        </a>
+        </Link>
 
         <div className="flex flex-col sm:flex-row sm:space-x-6">
-          <img
-            src="images/rooms/room-1.jpg"
-            alt="Grand Conference Hall"
+          <Image
+            src={`/images/rooms/${room.image}`}
+            alt={room.name}
+            width={400}
+            height={100}
             className="w-full sm:w-1/3 h-64 object-cover rounded-lg"
           />
 
           <div className="mt-4 sm:mt-0 sm:flex-1">
-            <p className="text-gray-600 mb-4">
-              Spacious conference hall perfect for large meetings and events.
-              Equipped with modern amenities and comfortable seating.
+            <p className="text-[#333] mb-4">
+              {room.description}
             </p>
 
             <ul className="space-y-2">
               <li>
-                <span className="font-semibold text-gray-800">Size:</span> 3000 sq ft
+                <span className="font-semibold text-[#111]">Size:</span> {room.sqft}
               </li>
               <li>
-                <span className="font-semibold text-gray-800">Availability:</span> 09:00-17:00
+                <span className="font-semibold text-[#111]">Availability:</span> {room.availability}
               </li>
               <li>
-                <span className="font-semibold text-gray-800">Price:</span> £150/hour
+                <span className="font-semibold text-[#111]">Price:</span> £{room.price_per_hour} per hour
               </li>
               <li>
-                <span className="font-semibold text-gray-800">Address:</span> 555
-                California St, San Francisco, CA 94104
+                <span className="font-semibold text-[#111]">Address:</span> {room.address}
               </li>
             </ul>
           </div>
         </div>
-
-        <div className="mt-6">
-          <h2 className="text-xl font-bold">Book this Room</h2>
-          <form className="mt-4">
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-              <div>
-                <label
-                  htmlFor="check_in_date"
-                  className="block text-sm font-medium text-[#111]"
-                >
-                  Check-In Date
-                </label>
-                <input
-                  type="date"
-                  id="check_in_date"
-                  name="check_in_date"
-                  className="mt-1 block w-full px-3 py-2 border border-[#333] rounded-md shadow-sm focus:ring-[#111] focus:border-[#111] sm:text-sm"
-                  required
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="check_in_time"
-                  className="block text-sm font-medium text-[#111]"
-                >
-                  Check-In Time
-                </label>
-                <input
-                  type="time"
-                  id="check_in_time"
-                  name="check_in_time"
-                  className="mt-1 block w-full px-3 py-2 border border-[#333] rounded-md shadow-sm focus:ring-[#111] focus:border-[#111] sm:text-sm"
-                  required
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="check_out_date"
-                  className="block text-sm font-medium text-[#111]"
-                >
-                  Check-Out Date
-                </label>
-                <input
-                  type="date"
-                  id="check_out_date"
-                  name="check_out_date"
-                  className="mt-1 block w-full px-3 py-2 border border-[#333] rounded-md shadow-sm focus:ring-[#111] focus:border-[#111] sm:text-sm"
-                  required
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="check_out_time"
-                  className="block text-sm font-medium text-[#111]"
-                >
-                  Check-Out Time
-                </label>
-                <input
-                  type="time"
-                  id="check_out_time"
-                  name="check_out_time"
-                  className="mt-1 block w-full px-3 py-2 border border-[#333] rounded-md shadow-sm focus:ring-[#111] focus:border-[#111] sm:text-sm"
-                  required
-                />
-              </div>
-            </div>
-
-            <div className="mt-6">
-              <button
-                type="submit"
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-[#fff] bg-black hover:bg-[#333] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#111]"
-              >
-                Book Room
-              </button>
-            </div>
-          </form>
-        </div>
+        <BookingForm />
       </div>
     </>
    );

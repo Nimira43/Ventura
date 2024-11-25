0c1f991415bd666e4d2fee1784cb2889bd6f0278
Nimira43 <n.rabone72@aol.com>
2024-11-25 23:58:49 +0000

    Updated.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,6 +4,7 @@ const currenYear = new Date().getFullYear()
   return ( 
     <footer className='bg-[#111] py-6'>
       <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+        
         <p className='text-center text-sm text-[#f5f5dc]'>
           &copy; { currenYear } Ventura Venues. All rights reserved.
         </p>

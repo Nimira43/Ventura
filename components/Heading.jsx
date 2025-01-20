495cc775448bd8234ec9fb7b5fb936d94106e8d7
Nimira43 <n.rabone72@aol.com>
2025-01-20 16:56:56 +0000

    New components setup with basic code. All archived component code now in the new relevant ones - commented out for now.

---
+++ components/Heading.jsx
@@ -0,0 +1,21 @@
+const Heading = () => {
+  return (
+    <div>
+      Heading
+    </div>
+  )
+}
+
+export default Heading
+
+// const Heading = ({title}) => {
+//   return (  
+//     <section className="bg-[#fff] mb-5 shadow px-4 py-4">
+//         <h1 className="text-2xl font-bold tracking-tight text-[#111]">
+//           { title }
+//         </h1>
+//     </section>
+//   )
+// }
+ 
+// export default Heading;

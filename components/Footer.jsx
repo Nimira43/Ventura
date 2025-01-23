67d00ae6476cddd0b9949854236d9ff0e49d9837
Nimira43 <n.rabone72@aol.com>
2025-01-23 06:48:37 +0000

    Footer redesigned

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,26 +1,15 @@
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <footer className='py-6 bg-grey-md'>
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <p className="text-center text-sm text-light">
+        <span className='logo text-xl'>Ventura Venues </span>&copy; 2025, NimiraTech. All rights reserved.
+      </p>
     </div>
+  </footer>
   )
 }
 
 export default Footer
 
-// const Footer = () => {
-//   const currenYear = new Date().getFullYear()
-  
-//     return ( 
-//       <footer className='bg-[#111] py-6'>
-//         <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
-          
-//           <p className='text-center text-sm text-[#f5f5dc]'>
-//             &copy; { currenYear } Ventura Venues. All rights reserved.
-//           </p>
-//         </div>
-//       </footer>
-//      )
-//   }
-   
-//   export default Footer
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-  "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  "./app/**/*.{js,ts,jsx,tsx,mdx}",
-];
-export const theme = {
-  extend: {
-    colors: {
-      'primary': '#5286e1',
-      'primary-lg': '#c4eeff',
-      'dark': '#111',
-      'grey-dk': '#333',
-      'grey-md': '#888',
-      'grey-lg': '#ccc',
-      'grey-xl': '#eee',
-      'lighter-blue' : '#edf7ff',
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#5286e1',
+        'primary-lg': '#c4eeff',
+        'dark': '#111',
+        'grey-dk': '#333',
+        'grey-md': '#888',
+        'grey-lg': '#ccc',
+        'grey-xl': '#eee',
+        'lighter-blue': '#edf7ff',
+        'light': '#ffffee'
+      },
     },
   },
+  plugins: [],
 };
-export const plugins = [];
+

b46a2fc957323bdae312caef1c3744bd11c1aeaa
Nimira43 <n.rabone72@aol.com>
2025-02-08 23:00:49 +0000

    Brought all the code back in to Home page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,9 +2,9 @@ const Footer = () => {
 const currentYear = new Date().getFullYear()
 
   return (
-    <footer className='py-6 bg-grey-md'>
+    <footer className='py-6'>
     <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
-      <p className='text-center text-sm text-light'>
+      <p className='text-center text-sm text-dark'>
         <span className='logo text-xl'>Ventura Venues </span>&copy; {currentYear}, NimiraTech. All rights reserved.
       </p>
     </div>

d716bac866c27f872e2f96779f6c846e87de4858
Nimira43 <n.rabone72@aol.com>
2025-02-08 22:50:07 +0000

    Using currentYear variable for the year value in the footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,11 @@
 const Footer = () => {
+const currentYear = new Date().getFullYear()
+
   return (
     <footer className='py-6 bg-grey-md'>
     <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
       <p className='text-center text-sm text-light'>
-        <span className='logo text-xl'>Ventura Venues </span>&copy; 2025, NimiraTech. All rights reserved.
+        <span className='logo text-xl'>Ventura Venues </span>&copy; {currentYear}, NimiraTech. All rights reserved.
       </p>
     </div>
   </footer>

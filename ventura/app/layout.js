654e75bef8b29f1d3c296d08a5e050bbaa1c6b19
Nimira43 <n.rabone72@aol.com>
2025-03-17 18:02:37 +0000

    Created new Next JS boilerplate. Cleaned that up and brought in assets and data files from archive.

--- ventura/app/layout.js
+++ ventura/app/layout.js
@@ -1,6 +1,5 @@
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
 import { Poppins } from 'next/font/google'
+
 import '@/assets/styles/globals.css'
 
 const poppins = Poppins({
@@ -17,13 +16,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={poppins.className}>
-        <Header />
-        <main className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
          {children}         
-        </main> 
-        <Footer /> 
       </body>
     </html>
-  );
+  )
 }
-

2c06dce41398a7dc7fd2adc14c3bac4cda6a6036
Nimira43 <n.rabone72@aol.com>
2025-03-17 19:37:08 +0000

    Brought back Header

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Poppins } from 'next/font/google'
 import '@/assets/styles/globals.css'
+import Header from '@/components/Header'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={poppins.className}>
+        <Header />
         <main className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
           {children}
         </main>
---
+++ components/Header.jsx
@@ -0,0 +1,95 @@
+import Link from 'next/link'
+import { AiOutlineUser } from 'react-icons/ai'
+import { PiSignInLight, PiSignOutLight } from 'react-icons/pi'
+import { LiaBuilding } from 'react-icons/lia'
+
+const Header = () => {
+  return (
+    <header className='bg-primary-lg'>
+      <nav className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>         
+        <div className='flex h-16 items-center justify-between'>
+          <div className='flex items-center'>
+            <Link href='/'>
+              <h1 className='text-3xl font-medium logo'>vv</h1>
+            </Link>
+            <div className='hidden md:block'>
+              <div className='ml-10 flex items-baseline space-x-4'>
+                <Link
+                  href='/'
+                  className='rounded-md px-3 py-2 text-sm  text-dark hover:bg-primary hover:text-white'
+                >
+                  Rooms
+                </Link>
+                <Link
+                  href='/bookings'
+                  className='rounded-md px-3 py-2 text-sm  text-dark   hover:bg-primary hover:text-white'
+                >
+                  Bookings
+                </Link>
+                <Link
+                  href='/add-room'
+                  className='rounded-md px-3 py-2 text-sm  text-dark hover:bg-primary hover:text-white'
+                >
+                  Add Room
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className='ml-auto'>
+            <div className='ml-4 flex items-center md:ml-6'>
+              <Link
+                href='login'
+                className='mr-3 uppercase text-dark hover:text-primary'
+              >
+                <PiSignInLight className='inline mr-1'/> Login
+              </Link>
+              <Link
+                href='/register'
+                className='mr-3 uppercase text-dark hover:text-primary'
+              >
+                <AiOutlineUser className='inline mr-1'/> Register
+              </Link>
+              <Link 
+                href='my-rooms.html'
+                className='mr-3 uppercase text-dark hover:text-primary'
+              >
+                <LiaBuilding className='inline mr-1'/> My Rooms
+              </Link>
+              <Link
+                href='/login'
+                className='mx-3 uppercase text-dark hover:text-primary'
+              >
+                <PiSignOutLight className='inline mr-1'/> Sign Out
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <div className='md:hidden'>
+        <div className='space-y-1 px-2 pb-3 pt-2 sm:px-3'>
+          <Link
+            href='/'
+            className='block rounded-md px-3 py-2 text-base uppercase text-dark hover:bg-primary hover:text-white'
+          >
+            Rooms
+          </Link>
+          <Link
+            href='/bookings'
+            className='block rounded-md px-3 py-2 text-base uppercase text-dark hover:bg-primary hover:text-white'
+          >
+            Bookings
+          </Link>
+          <Link
+            href='/add-room'
+            className='block rounded-md px-3 py-2 text-base uppercase text-dark hover:bg-primary hover:text-white'
+          >
+            Add Room
+          </Link>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header

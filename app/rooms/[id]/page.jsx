80fcbb8e598ed80b86c1a5fc76b0afbaadfc0d7a
Nimira43 <n.rabone72@aol.com>
2025-03-17 20:52:52 +0000

    Recreated rooms/[id]/page.tsx

---
+++ app/rooms/[id]/page.jsx
@@ -0,0 +1,16 @@
+import Heading from '@/components/Heading'
+import rooms from '@/data/rooms.json'
+
+const RoomPage = ({ params }) => {
+  const { id } = params
+  const room = rooms.find((room) => room.$id === id)
+
+  if (!room) {
+    return <Heading title='Room Not Found'/>
+  }
+  return ( 
+    <Heading title={room.name}/>
+   )
+}
+ 
+export default RoomPage

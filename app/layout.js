df44dbfeed808ff86d164e9cc58f22b8ca482a86
Nimira43 <n.rabone72@aol.com>
2025-03-17 18:39:45 +0000

    Added small and large screen padding.

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={poppins.className}>
-        <main className='mx-auto max-w-7xl px-4 py-6'>
+        <main className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
           {children}
         </main>
       </body>

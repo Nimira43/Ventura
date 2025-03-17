c79f876c4d09fbbb54b8e322539bb961c4ef445a
Nimira43 <n.rabone72@aol.com>
2025-03-17 20:13:36 +0000

    Import the rooms.json data into Home page.

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,11 @@
+import rooms from '@/data/rooms.json'
+
 export default function Home() {
   return (
     <>
-      <h1>Ventura</h1>
+      {rooms.length > 0 ? (
+        rooms.map((room) => <h3>{ room.name}</h3>)
+      ) : (<p>No Rooms</p>)}
     </>
   )
 }

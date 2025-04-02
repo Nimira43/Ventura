34d560c3eb3d813ee5b677c27d51eb199dd54605
Nimira43 <n.rabone72@aol.com>
2025-04-03 00:50:00 +0100

    Structuring client for different contexts, like admin-level access and user-session-based access

--- config/appwrite.js
+++ config/appwrite.js
@@ -8,14 +8,26 @@ const createAdminClient = async () => {
   return {
     get account() {
       return new Account(client)
-    }
+    },
     get databases() {
       return new Databases(client)
-    }
-    get account() {
+    },
+    get storage() {
       return new Storage(client)
-    }
+    },
   }
 }
 
-const createSessionsClient = async (session) => {}
+const createSessionClient = async (session) => {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT)
+  return {
+    get account() {
+      return new Account(client)
+    },
+    get databases() {
+      return new Databases(client)
+    },
+  }
+}

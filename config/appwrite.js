4386ab8bddc7fe4a5ad17e368f6c6089298cdd38
Nimira43 <n.rabone72@aol.com>
2025-04-03 00:18:00 +0100

    Passed client into Account

--- config/appwrite.js
+++ config/appwrite.js
@@ -5,6 +5,11 @@ const createAdminClient = async () => {
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT)
     .setKey(process.env.NEXT_APPWRITE_KEY)
+  return {
+    get account() {
+      return new Account(client)
+    }
+  }
 }
 
 const createSessionsClient = async (session) => {}

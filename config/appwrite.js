2ab8188465bff0b1f98da2114d30b6c0619b9be8
Nimira43 <n.rabone72@aol.com>
2025-04-03 00:52:53 +0100

    Including if (session) to set the session dynamically. It ensures Appwrite client is versatile for handling authenticated user sessions.

--- config/appwrite.js
+++ config/appwrite.js
@@ -22,6 +22,11 @@ const createSessionClient = async (session) => {
   const client = new Client()
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT)
+  
+  if (session) {
+    client.setSession(session)
+  }
+  
   return {
     get account() {
       return new Account(client)

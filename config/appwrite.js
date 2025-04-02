9fd309c2979d0041034eb76b6ac6dde0d395f606
Nimira43 <n.rabone72@aol.com>
2025-04-03 00:16:52 +0100

    Created createAdminClient and with new Client instance.

--- config/appwrite.js
+++ config/appwrite.js
@@ -1,2 +1,10 @@
 import { Client, Databases, Account, Storage } from 'node-appwrite'
 
+const createAdminClient = async () => {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT)
+    .setKey(process.env.NEXT_APPWRITE_KEY)
+}
+
+const createSessionsClient = async (session) => {}

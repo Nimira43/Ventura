3c65a55938a009c3b35693867f045ea8f1295e07
Nimira43 <n.rabone72@aol.com>
2025-04-26 16:16:50 +0100

    Added key.

--- app/page.jsx
+++ app/page.jsx
@@ -4,12 +4,12 @@ import getAllRooms from './actions/getAllRooms'
 
 export default async function Home() {
   const rooms = await getAllRooms()
-  
+
   return (
     <>
       <Heading title='Available Rooms' />
       {rooms.length > 0 ? (
-        rooms.map((room) => <RoomCard room={room} />)
+        rooms.map((room) => <RoomCard room={room} key={room.$id} />)
       ) : (<p>No Rooms</p>)}
     </>
   )

b4110b1cb14ea3246e1ca6c9d87178a893b5bbc2
Nimira43 <n.rabone72@aol.com>
2025-04-26 16:05:57 +0100

    retrieve all room documents from Appwrite database.

--- app/actions/getAllRooms.js
+++ app/actions/getAllRooms.js
@@ -7,6 +7,11 @@ import { redirect } from 'next/navigation'
 async function getAllRooms() {
   try {
     const { databases } = await createAdminClient()
+    const { documents: rooms } = await databases.listDocuments(
+      process.env.NEXT_PUBLIC_APPWRITE_DATABASE,
+      process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ROOMS
+    )
+  
   } catch (error) {
 
   }

757a7b9f72ebcf2526b59d541bbae121dfdac77f
Nimira43 <n.rabone72@aol.com>
2025-07-02 12:28:35 +0100

    Middleware 4

--- middleware.js
+++ middleware.js
@@ -3,5 +3,7 @@ import { NextResponse } from 'next/server'
 export async function middleware(request) {
   const { pathname } = request.nextUrl
 
-  
+  console.log(`Requested page: ${pathname}`)
+ 
+  return NextResponse.next()
 }

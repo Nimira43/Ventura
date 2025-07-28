178756969a04e9500ac025a344f8456a97a078a6
Nimira43 <n.rabone72@aol.com>
2025-07-28 04:04:39 +0100

    Created middleware function

---
+++ middleware.js
@@ -0,0 +1,9 @@
+import { NextResponse } from  'next/server'
+
+export async function middleware(request) {
+  const { pathname } = request.nextUrl
+
+  console.log(`Requested Page: ${pathname}`)
+
+  return NextResponse.next()
+}

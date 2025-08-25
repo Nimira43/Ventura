3a0e72630613d40d72cf83c7201e40d0df26a4a8
Nimira43 <n.rabone72@aol.com>
2025-08-25 22:31:56 +0100

    Added previousState parameter to createSession function

--- app/actions/createSession.js
+++ app/actions/createSession.js
@@ -1,6 +1,6 @@
 'use server'
 
-async function createSession(formData) {
+async function createSession(previousState, formData) {
   const email = formData.get('email')
   const password = formData.get('password')
   console.log(email, password)

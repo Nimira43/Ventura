674d7b88809bcf058a15120747e879d4825d139f
Nimira43 <n.rabone72@aol.com>
2025-09-08 05:55:57 +0100

    Retrieve sessionCookie

--- app/actions/destroySession.js
+++ app/actions/destroySession.js
@@ -3,6 +3,13 @@ import { createSessionClient } from '@/config/appwrite'
 import { cookies } from 'next/headers'
 
 async function destroySession() {
+  const sessionCookie = cookies().get('appwrite-session')
+
+  if (!sessionCookie) {
+    return {
+      error: 'No session cookie found'
+    } 
+  }
   try {
     
   } catch (error) {   

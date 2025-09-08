dc06082246eb237bbc4ebf617f68860380a1c772
Nimira43 <n.rabone72@aol.com>
2025-09-08 05:53:36 +0100

    Cleaned up code in destroySession

--- app/actions/destroySession.js
+++ app/actions/destroySession.js
@@ -1,37 +1,15 @@
 'use server'
-import { createAdminClient } from '@/config/appwrite'
+import { createSessionClient } from '@/config/appwrite'
 import { cookies } from 'next/headers'
 
-async function createSession(previousState, formData) {
-  const email = formData.get('email')
-  const password = formData.get('password')
-
-  if (!email || !password) {
-    return {
-      error: 'Please fill out all fields.'
-    }
-  }
-
-  const { account } = await createAdminClient()
-
+async function destroySession() {
   try {
-    const session = await account.createEmailPasswordSession(email, password)
-    cookies().set('appwrite-session', session.secret, {
-      httpOnly: true,
-      secure: true,
-      sameSite: 'strict',
-      expires: new Date(session.expire),
-      path: '/'
-    })
+    
+  } catch (error) {   
     return {
-      success: true
-    }
-  } catch (error) {
-    console.log('Authentication Error: ', error)    
-    return {
-      error: 'Invalid Credentials.'
+      error: 'Error deleting session.'
     }
   }
 }
 
-export default createSession
+export default destroySession

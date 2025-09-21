273806ac2a0e8f9bc431e8adbe20b75664b1f07f
Nimira43 <n.rabone72@aol.com>
2025-09-21 13:45:03 +0100

    checkAuth async function  - 3

--- app/actions/checkAuth.js
+++ app/actions/checkAuth.js
@@ -5,6 +5,12 @@ import { cookies } from 'next/headers'
 
 async function checkAuth() {
   const sessionCookie = cookies().get('appwrite-session')
+
+  if (!sessionCookie) {
+    return {
+      isAuthenticated: false
+    }
+  }
 }
 
 export default checkAuth

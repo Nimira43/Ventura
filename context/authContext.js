d95255705f624afb00e9e08162677fda6bec008a
Nimira43 <n.rabone72@aol.com>
2025-09-21 16:04:47 +0100

    useAuth - 3

--- context/authContext.js
+++ context/authContext.js
@@ -34,5 +34,7 @@ export const AuthProvider = ({children}) => {
 export const useAuth = () => {
   const context = useContext(AuthContext)
 
-  
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider.')
+  }
 }

fe7a0a1c295727672ca57b73b086a7ba494f3689
Nimira43 <n.rabone72@aol.com>
2025-10-04 12:38:11 +0100

    createUser function - 2

--- app/actions/createUser.js
+++ app/actions/createUser.js
@@ -3,7 +3,10 @@
 import { createAdminClient } from '@/config/appwrite'
 import { ID } from 'node-appwrite'
 
-async function createUser() {
+async function createUser(previousState, formData) {
+  const name = form.Data.get('name')
+  const email = form.Data.get('email')
+  const password = form.Data.get('password')
 
 }
 

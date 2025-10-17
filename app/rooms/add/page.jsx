a8c56848ea7ea80b0d7d68560c152e7c729b3d2f
Nimira43 <n.rabone72@aol.com>
2025-10-17 21:00:47 +0100

    Copied a label input field multiple times from the login page

--- app/rooms/add/page.jsx
+++ app/rooms/add/page.jsx
@@ -7,7 +7,154 @@ const AddRoomPage = () => {
       <div className='bg-light shadow-lg rounded-lg p-6 w-full'>
         <form>
           <div className='mb-4'>
-            
+            <label
+              htmlFor='email'
+              className='block text-dark font-normal mb-2'
+            >
+              Email
+            </label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              className='border rounded w-full py-2 px-3'
+              required
+            />
+          </div>
+          <div className='mb-4'>
+            <label
+              htmlFor='email'
+              className='block text-dark font-normal mb-2'
+            >
+              Email
+            </label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              className='border rounded w-full py-2 px-3'
+              required
+            />
+          </div>
+          <div className='mb-4'>
+            <label
+              htmlFor='email'
+              className='block text-dark font-normal mb-2'
+            >
+              Email
+            </label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              className='border rounded w-full py-2 px-3'
+              required
+            />
+          </div>
+          <div className='mb-4'>
+            <label
+              htmlFor='email'
+              className='block text-dark font-normal mb-2'
+            >
+              Email
+            </label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              className='border rounded w-full py-2 px-3'
+              required
+            />
+          </div>
+          <div className='mb-4'>
+            <label
+              htmlFor='email'
+              className='block text-dark font-normal mb-2'
+            >
+              Email
+            </label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              className='border rounded w-full py-2 px-3'
+              required
+            />
+          </div>
+          <div className='mb-4'>
+            <label
+              htmlFor='email'
+              className='block text-dark font-normal mb-2'
+            >
+              Email
+            </label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              className='border rounded w-full py-2 px-3'
+              required
+            />
+          </div>
+          <div className='mb-4'>
+            <label
+              htmlFor='email'
+              className='block text-dark font-normal mb-2'
+            >
+              Email
+            </label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              className='border rounded w-full py-2 px-3'
+              required
+            />
+          </div>
+          <div className='mb-4'>
+            <label
+              htmlFor='email'
+              className='block text-dark font-normal mb-2'
+            >
+              Email
+            </label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              className='border rounded w-full py-2 px-3'
+              required
+            />
+          </div>
+          <div className='mb-4'>
+            <label
+              htmlFor='email'
+              className='block text-dark font-normal mb-2'
+            >
+              Email
+            </label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              className='border rounded w-full py-2 px-3'
+              required
+            />
+          </div>
+          <div className='mb-4'>
+            <label
+              htmlFor='email'
+              className='block text-dark font-normal mb-2'
+            >
+              Email
+            </label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              className='border rounded w-full py-2 px-3'
+              required
+            />
           </div>
         </form>
       </div>

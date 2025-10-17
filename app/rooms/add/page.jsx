7a7120407f9f21294b118c38e387c9ecffd7d2f7
Nimira43 <n.rabone72@aol.com>
2025-10-17 18:25:01 +0100

    AddRoomPage form - 1

--- app/rooms/add/page.jsx
+++ app/rooms/add/page.jsx
@@ -4,7 +4,13 @@ const AddRoomPage = () => {
   return ( 
     <>
       <Heading title='Add Room' />
-      
+      <div className='bg-light shadow-lg rounded-lg p-6 w-full'>
+        <form>
+          <div className='mb-4'>
+            
+          </div>
+        </form>
+      </div>
     </>
    )
 }

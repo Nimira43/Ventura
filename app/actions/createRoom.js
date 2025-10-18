13915897514c3b735053862fe7f3cdb9feb2095e
Nimira43 <n.rabone72@aol.com>
2025-10-18 08:03:43 +0100

    createRoom - 2

--- app/actions/createRoom.js
+++ app/actions/createRoom.js
@@ -5,6 +5,20 @@ import checkAuth from './checkAuth'
 import { ID } from 'node-appwrite'
 import { revalidatePath } from 'next/cache'
 
-async function createRoom(previousState, formData) {}
+async function createRoom(previousState, formData) {
+  const { databases } = await createAdminClient()
+  try {
+    const { user } = await checkAuth()
+
+    if (!user) {
+      return {
+        error: 'You must be logged in to create a room'
+      }
+    }
+  } catch (error) {
+    
+  }
+
+}
 
 export default createRoom

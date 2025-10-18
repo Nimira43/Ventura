a483e1ebc88e00940b8e30ae8470418ef0bffd2d
Nimira43 <n.rabone72@aol.com>
2025-10-18 07:56:10 +0100

    createRoom - 1

--- app/actions/createRoom.js
+++ app/actions/createRoom.js
@@ -4,3 +4,7 @@ import { createAdminClient } from '@/config/appwrite'
 import checkAuth from './checkAuth'
 import { ID } from 'node-appwrite'
 import { revalidatePath } from 'next/cache'
+
+async function createRoom(previousState, formData) {}
+
+export default createRoom

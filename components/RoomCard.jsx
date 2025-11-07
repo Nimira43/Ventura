610fa7e797d350acacfd0e7af80cfa1e1494d41d
Nimira43 <n.rabone72@aol.com>
2025-11-07 12:34:46 +0000

    projectId

--- components/RoomCard.jsx
+++ components/RoomCard.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const RoomCard = ({ room }) => {
   const bucketId = process.env.NEXT_PUBLIC_APPWRITE_STORAGE_BUCKET_ROOMS
-  
+  const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT
 
   return ( 
     <div

6ef8de2ecfd9eb8c369bc310c7a4a018a8a019a2
Nimira43 <n.rabone72@aol.com>
2025-11-24 08:27:28 +0000

    Copied over bucket / image variables from RoomCard into RoomPage

--- app/rooms/[id]/page.jsx
+++ app/rooms/[id]/page.jsx
@@ -12,6 +12,13 @@ const RoomPage = async ({ params }) => {
   if (!room) {
     return <Heading title='Room Not Found'/>
   }
+
+  const bucketId = process.env.NEXT_PUBLIC_APPWRITE_STORAGE_BUCKET_ROOMS
+  const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT
+
+  const imageUrl = `https://cloud.appwrite.io/v1/storage/buckets/${bucketId}/files/${room.image}/view?project=${projectId}`
+  const imageSrc = room.image ? imageUrl : '/images/no-image.jpg'
+
   return ( 
     <>
       <Heading title={room.name} />

72cd273a9a3c07064531a3284a42434222a9cd6f
Nimira43 <n.rabone72@aol.com>
2025-11-24 15:46:45 +0000

    MyRoomCard markup -3

--- components/MyRoomCard.jsx
+++ components/MyRoomCard.jsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { HiOutlineEye,  HiOutlineTrash} from 'react-icons/hi'
 
 const MyRoomCard = () => {
   return ( 
@@ -8,14 +9,16 @@ const MyRoomCard = () => {
       </div>
       <div className='flex flex-col sm:flex-row w-full sm:w-auto sm:space-x-2 mt-2 sm:mt-0'>
         <Link
-          to='#'
+          href='#'
           className='bg-primary-light hover:bg-primary text-dark hover:text-light px-4 py-2 rounded mb-2 sm:mb-0 w-full sm:w-auto text-center  uppercase font-medium btn-hover' 
-        />
+        >
+          <HiOutlineEye />
+        </Link>
         <button
           type='submit'
           className='w-full flex justify-center bg-primary-light text-dark hover:text-light px-4 py-2 rounded hover:bg-primary uppercase font-medium btn-hover'
         >
-          Book Room
+          <HiOutlineTrash />
         </button>
       </div>
     </div>

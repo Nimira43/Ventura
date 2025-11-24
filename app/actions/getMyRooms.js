9868c92ca77cdc1273845ede42e1392a1efbe0c5
Nimira43 <n.rabone72@aol.com>
2025-11-24 08:41:09 +0000

    Destructuring account and databases

--- app/actions/getMyRooms.js
+++ app/actions/getMyRooms.js
@@ -13,7 +13,7 @@ async function getMyRooms() {
   }
 
   try {
-    const { databases } = await createAdminClient()
+    const { account, databases } = await createSessionClient(sessionCookie.value)
     const { documents: rooms } = await databases.listDocuments(
       process.env.NEXT_PUBLIC_APPWRITE_DATABASE,
       process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ROOMS

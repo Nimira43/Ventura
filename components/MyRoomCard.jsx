fa2e12bcb9acd46069356762390401ed2c080de5
Nimira43 <n.rabone72@aol.com>
2025-11-24 14:23:52 +0000

    MyRoomCard markup - 1

--- components/MyRoomCard.jsx
+++ components/MyRoomCard.jsx
@@ -1,6 +1,11 @@
 const MyRoomCard = () => {
   return ( 
-    
+    <div className='bg-light shadow rounded-md p-4 mt-4 flex flex-col sm:flex-row justify-between items-center'>
+      <div className='flex flex-col'>
+        <h4 className='text-lg font-medium'>My Room 1</h4>
+      </div>
+      
+    </div>
   )
 }
  

2b2613e0e1a933987883439dbe8d54b72407ff09
Nimira43 <n.rabone72@aol.com>
2025-12-10 04:06:21 +0000

    Added onClick

--- components/DeleteRoomButton.jsx
+++ components/DeleteRoomButton.jsx
@@ -7,6 +7,7 @@ import deleteRoom from '@/app/actions/deleteRoom'
 const DeleteRoomButton = () => {
   return ( 
     <button
+      onClick={handleDelete}
       className='centre main-btn mb-2 sm:mb-0 w-full sm:w-auto text-center'
     >
       <HiOutlineTrash className='mr-2 text-lg' /> Delete

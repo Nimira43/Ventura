0d6d2d025c2cabcfeeb1d9da8d21a8a852e0be09
Nimira43 <n.rabone72@aol.com>
2025-12-27 16:41:09 +0000

    Updated catch errors

--- app/actions/bookRoom.js
+++ app/actions/bookRoom.js
@@ -24,15 +24,11 @@ async function bookRoom(previousState, formData) {
       }
     }
 
-    const { documents: rooms } = await databases.listDocuments(
-      process.env.NEXT_PUBLIC_APPWRITE_DATABASE,
-      process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ROOMS,
-      [Query.equal('user_id', userId)]
-    )
-    return rooms 
   } catch (error) {
-    console.log('Failed to get user rooms', error)
-    redirect('/error')
+    console.log('Failed to get book room', error)
+    return {
+      error: 'Something went wrong with booking the room.'
+    }
   }
 }
 

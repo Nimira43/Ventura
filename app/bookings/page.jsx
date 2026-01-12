44ee3a4e54acdd04a84fbd05273fb5bb3ed00ff1
Nimira43 <n.rabone72@aol.com>
2026-01-12 21:16:17 +0000

    Created getMyBookings action. For the moment just display booking name on BookingsPage

--- app/bookings/page.jsx
+++ app/bookings/page.jsx
@@ -1,7 +1,22 @@
-const BookingsPage = () => {
+import Heading from '@/components/Heading'
+import getMyBookings from '../actions/getMyBookings'
+
+const BookingsPage = async () => {
+  const bookings = await getMyBookings()
+  
   return ( 
     <>
-      Bookings
+      {bookings.length === 0
+        ? (
+            <p className='text-dark mt-4'>You have no bookings.</p>
+        )
+        : (
+            bookings.map((booking) => (
+              <h3>{booking.room_id.name}</h3>
+            )
+          )
+        )
+      }
     </>
   )
 }
